5c4f243e59b597f761c30e75a81087fba5aa6317
Gabriele Genta <gabriele@adespresso.com>
2017-08-16 15:46:47 +0200

    Setup project using create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default () => (
+    <div className="App">
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React Redux Course</h2>
+        </div>
+        <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        {/* Your code goes here! */}
+    </div>
+);
